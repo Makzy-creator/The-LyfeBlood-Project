550f80dff0143626b6af6301b0bbab38826c7691
Favour Chikezie <keichamaxwell@gmail.com>
2026-07-17 13:17:26 +0100

    update

--- web/react-router.config.ts
+++ web/react-router.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from '@react-router/dev/config';
+import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
 	appDirectory: './src/app',
 	ssr: true,
+	presets: [vercelPreset()],
 	future: {
 		v8_middleware: false,
 	},
